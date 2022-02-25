4f6ff2f52dde12cf00ea573f1fec22603736a074
Kel_BLK <70690676+Keltoum-BLK@users.noreply.github.com>
2022-02-25 12:01:09 +0100

    Game Form added

--- MyGamesLibrary/MyGamesLibrary/Controller/GameCardViewController.swift
+++ MyGamesLibrary/MyGamesLibrary/Controller/GameCardViewController.swift
@@ -47,11 +47,11 @@ class GameCardViewController: UIViewController {
         gameImage.addGradientLayerInBackground(frame: gameImage.bounds, colors: [UIColor(ciColor: .clear), UIColor(ciColor: .white)])
     }
     private func setUp(){
-        screenshotCollectionView.dataSource = self
-        screenshotCollectionView.delegate = self
+        //        screenshotCollectionView.dataSource = self
+        //        screenshotCollectionView.delegate = self
         //give data value to variables
         guard let finalGame = game else { return }
-//        setRating(for: finalGame.rating ?? 0)
+        //        setRating(for: finalGame.rating ?? 0)
         gameImage.cacheImage(urlString: finalGame.backgroundImage ?? "no image")
         gameTitle.text = finalGame.name ?? "no name"
         release_date.text = finalGame.released ?? "no date"
@@ -62,7 +62,7 @@ class GameCardViewController: UIViewController {
         backBTN.layer.cornerRadius = backBTN.frame.height / 2
         
         gameTitle.layer.cornerRadius = gameTitle.frame.height / 2
-        gameTitle.layer.masksToBounds = true 
+        gameTitle.layer.masksToBounds = true
         gameTitle.setMargins()
         gameTitle.textAlignment = .center
         
@@ -70,51 +70,58 @@ class GameCardViewController: UIViewController {
         xboxFavBTN.layer.cornerRadius = playstationFavBTN.frame.height / 2
         nintendoFavBTN.layer.cornerRadius = playstationFavBTN.frame.height / 2
         favBTN.layer.cornerRadius = playstationFavBTN.frame.height / 2
+        
+        addFavoriteStack.translatesAutoresizingMaskIntoConstraints = false
+        heart1.translatesAutoresizingMaskIntoConstraints = false
+        heart2.translatesAutoresizingMaskIntoConstraints = false
+        heart3.translatesAutoresizingMaskIntoConstraints = false
+        heart4.translatesAutoresizingMaskIntoConstraints = false
+        heart5.translatesAutoresizingMaskIntoConstraints = false
     }
     private func setUpHeart(image : UIImageView) {
-            image.image = UIImage(systemName: "heart.fill")
-            image.sizeThatFits(CGSize(width: 8, height: 8))
-            image.contentMode = .scaleAspectFit
-            image.translatesAutoresizingMaskIntoConstraints = true
+        image.image = UIImage(systemName: "heart.fill")
+        image.sizeThatFits(CGSize(width: 8, height: 8))
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = true
+    }
+    
+    //MARK: SetUp and method for update the rating star for each destination.
+    //condition to change the star's shape according to the rating
+    func setRating(for star : Double) {
+        setUpHeart(image: heart1)
+        setUpHeart(image: heart2)
+        setUpHeart(image: heart3)
+        setUpHeart(image: heart4)
+        setUpHeart(image: heart5)
+        let rating = Int(star)
+        switch rating {
+        case 0:
+            heart1.image = UIImage(systemName: "heart")
+            heart2.image = UIImage(systemName: "heart")
+            heart3.image = UIImage(systemName: "heart")
+            heart4.image = UIImage(systemName: "heart")
+            heart5.image = UIImage(systemName: "heart")
+        case 1:
+            heart2.image = UIImage(systemName: "heart")
+            heart3.image = UIImage(systemName: "heart")
+            heart4.image = UIImage(systemName: "heart")
+            heart5.image = UIImage(systemName: "heart")
+            
+        case 2:
+            heart3.image = UIImage(systemName: "heart")
+            heart4.image = UIImage(systemName: "heart")
+            heart5.image = UIImage(systemName: "heart")
+        case 3:
+            heart4.image = UIImage(systemName: "heart")
+            heart5.image = UIImage(systemName: "heart")
+        case 4:
+            heart5.image = UIImage(systemName: "heart")
+            
+        default:
+            break
         }
-        
-        //MARK: SetUp and method for update the rating star for each destination.
-        //condition to change the star's shape according to the rating
-        func setRating(for star : Double) {
-            setUpHeart(image: heart1)
-            setUpHeart(image: heart2)
-            setUpHeart(image: heart3)
-            setUpHeart(image: heart4)
-            setUpHeart(image: heart5)
-            let rating = Int(star)
-            switch rating {
-            case 0:
-                heart1.image = UIImage(systemName: "heart")
-                heart2.image = UIImage(systemName: "heart")
-                heart3.image = UIImage(systemName: "heart")
-                heart4.image = UIImage(systemName: "heart")
-                heart5.image = UIImage(systemName: "heart")
-            case 1:
-                heart2.image = UIImage(systemName: "heart")
-                heart3.image = UIImage(systemName: "heart")
-                heart4.image = UIImage(systemName: "heart")
-                heart5.image = UIImage(systemName: "heart")
-
-            case 2:
-                heart3.image = UIImage(systemName: "heart")
-                heart4.image = UIImage(systemName: "heart")
-                heart5.image = UIImage(systemName: "heart")
-            case 3:
-                heart4.image = UIImage(systemName: "heart")
-                heart5.image = UIImage(systemName: "heart")
-            case 4:
-                heart5.image = UIImage(systemName: "heart")
-
-            default:
-                break
-            }
-        }
-
+    }
+    
     private func setupConstraints() {
         //image constraints
         gameImage.backgroundImage(view: self.view, multiplier: 0.45)
@@ -132,9 +139,8 @@ extension GameCardViewController: UICollectionViewDelegate, UICollectionViewData
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Screenshots", for: indexPath) as! ScreenshotsViewCell
-        cell.gameImage.cacheImage(urlString: screenshots[indexPath.row])
-        return cell 
+        let cell = UICollectionViewCell()
+        return cell
     }
     
     
